deb42a524e2bcbfeeeb26f763c5614439a342530
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-26 10:54:33 +0530

    Comment out customer management functions and related code

--- js/function.js
+++ js/function.js
@@ -1,46 +1,63 @@
-let customerList = []
+// // let customerList = []
 
-function addCustomerOnAction() {
-    let name = document.getElementById("txtcustomerName").value;
-    let address = document.getElementById("txtAddress").value;
-    let age = document.getElementById("txtAge").value;
-    let email = document.getElementById("txtEmail").value;
-    let salary = document.getElementById("txtSalary").value;
+// // function addCustomerOnAction() {
+// //     let name = document.getElementById("txtcustomerName").value;
+// //     let address = document.getElementById("txtAddress").value;
+// //     let age = document.getElementById("txtAge").value;
+// //     let email = document.getElementById("txtEmail").value;
+// //     let salary = document.getElementById("txtSalary").value;
 
-    let customer = {
-        name: name,
-        address: address,
-        age: age,
-        email: email,
-        salary: salary
-    };
+// //     let customer = {
+// //         name: name,
+// //         address: address,
+// //         age: age,
+// //         email: email,
+// //         salary: salary
+// //     };
 
-    customerList.push(customer);
-    loadCustomersOnAction();
-    document.getElementById("txtcustomerName").value = "";
-    document.getElementById("txtAddress").value = "";
-    document.getElementById("txtAge").value = "";
-    document.getElementById("txtEmail").value = "";
-    document.getElementById("txtSalary").value = "";
-    console.log(customerList);
-}
+// //     customerList.push(customer);
+// //     loadCustomersOnAction();
+// //     document.getElementById("txtcustomerName").value = "";
+// //     document.getElementById("txtAddress").value = "";
+// //     document.getElementById("txtAge").value = "";
+// //     document.getElementById("txtEmail").value = "";
+// //     document.getElementById("txtSalary").value = "";
+// //     console.log(customerList);
+// // }
 
-function loadCustomersOnAction() {
-    let tblCustomers = document.getElementById("tblCustomer");
+// // function loadCustomersOnAction() {
+// //     let tblCustomers = document.getElementById("tblCustomer");
 
-    let body = "";
+// //     let body = "";
 
-    for (let i = 0; i < customerList.length; i++) {
-        body += `<tr>
-            <td>${customerList[i].name}</td>
-            <td>${customerList[i].address}</td>
-            <td>${customerList[i].age}</td>
-            <td>${customerList[i].email}</td>
-            <td>${customerList[i].salary}</td>
-        </tr> `;
+// //     for (let i = 0; i < customerList.length; i++) {
+// //         body += `<tr>
+// //             <td>${customerList[i].name}</td>
+// //             <td>${customerList[i].address}</td>
+// //             <td>${customerList[i].age}</td>
+// //             <td>${customerList[i].email}</td>
+// //             <td>${customerList[i].salary}</td>
+// //         </tr> `;
 
-        tblCustomers.innerHTML = body;
-    }
-        console.log(tblCustomers);
+// //         tblCustomers.innerHTML = body;
+// //     }
+// //         console.log(tblCustomers);
 
-}
+// // }
+
+// const name = "Pathum";
+
+// name = "jagath";
+
+let name = "Pathum";
+let age = 25;
+let isMe = true;
+let empty;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMe);
+console.log(typeof empty);
+
+
+
