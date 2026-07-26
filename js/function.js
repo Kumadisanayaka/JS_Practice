50602e21e78c271cffb58954d7b432cf7eaed96e
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-26 10:54:24 +0530

    Add customer management functions and initialize customer list

---
+++ js/function.js
@@ -0,0 +1,46 @@
+let customerList = []
+
+function addCustomerOnAction() {
+    let name = document.getElementById("txtcustomerName").value;
+    let address = document.getElementById("txtAddress").value;
+    let age = document.getElementById("txtAge").value;
+    let email = document.getElementById("txtEmail").value;
+    let salary = document.getElementById("txtSalary").value;
+
+    let customer = {
+        name: name,
+        address: address,
+        age: age,
+        email: email,
+        salary: salary
+    };
+
+    customerList.push(customer);
+    loadCustomersOnAction();
+    document.getElementById("txtcustomerName").value = "";
+    document.getElementById("txtAddress").value = "";
+    document.getElementById("txtAge").value = "";
+    document.getElementById("txtEmail").value = "";
+    document.getElementById("txtSalary").value = "";
+    console.log(customerList);
+}
+
+function loadCustomersOnAction() {
+    let tblCustomers = document.getElementById("tblCustomer");
+
+    let body = "";
+
+    for (let i = 0; i < customerList.length; i++) {
+        body += `<tr>
+            <td>${customerList[i].name}</td>
+            <td>${customerList[i].address}</td>
+            <td>${customerList[i].age}</td>
+            <td>${customerList[i].email}</td>
+            <td>${customerList[i].salary}</td>
+        </tr> `;
+
+        tblCustomers.innerHTML = body;
+    }
+        console.log(tblCustomers);
+
+}
